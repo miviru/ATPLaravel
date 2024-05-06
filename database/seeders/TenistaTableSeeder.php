dbe62092efd769b9e82ef003cfbebc1f7a397749
miviru <miviru98@hotmail.com>
2024-05-06 23:50:39 +0200

    seeder Tenista y Torneo

---
+++ database/seeders/TenistaTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tenista;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TenistaTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Tenista::create([
+            'puntos'=> 100,
+            'nombre' => 'Rafael Nadal',
+            'pais' => 'España',
+            'fecha_nacimiento' => '1986-06-03',
+            'altura' => 185,
+            'peso' => 85,
+            'profesional_desde' => 2001,
+            'mano' => 'ZURDO',
+            'reves' => 'DOS_MANOS',
+            'entrenador' => 'Carlos Moya',
+            'ganancias' => 123000000,
+            'mejor_ranking' => 1,
+            'victorias' => 1000,
+            'derrotas' => 200,
+        ]);
+
+        Tenista::create([
+            'puntos'=> 90,
+            'nombre' => 'Roger Federer',
+            'pais' => 'Suiza',
+            'fecha_nacimiento' => '1981-08-08',
+            'altura' => 185,
+            'peso' => 85,
+            'profesional_desde' => 1998,
+            'mano' => 'DIESTRO',
+            'reves' => 'UNA_MANO',
+            'entrenador' => 'Ivan Ljubicic',
+            'ganancias' => 129000000,
+            'mejor_ranking' => 1,
+            'victorias' => 1200,
+            'derrotas' => 300,
+        ]);
+    }
+}
---
+++ database/seeders/TorneoTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Torneo;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TorneoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+      Torneo::create([
+        'nombre' => 'Master 100 Madrid',
+        'ubicacion' => 'Madrid, España',
+        'modo' => 'AMBOS',
+        'categoria' => 'ATP_1000',
+        'superficie' => 'ARCILLA',
+        'entradas_individual' => 128,
+        'entradas_dobles' => 64,
+        'premio' => 34000000,
+        'puntos' => 1000,
+        'fecha_inicio' => '2021-06-28',
+        'fecha_fin' => '2021-07-11',
+      ]);
+
+        Torneo::create([
+            'nombre' => 'Master 1000 Roma',
+            'ubicacion' => 'Roma, Italia',
+            'modo' => 'AMBOS',
+            'categoria' => 'ATP_1000',
+            'superficie' => 'ARCILLA',
+            'entradas_individual' => 128,
+            'entradas_dobles' => 64,
+            'premio' => 34000000,
+            'puntos' => 1000,
+            'fecha_inicio' => '2021-05-09',
+            'fecha_fin' => '2021-05-16',
+        ]);
+}
